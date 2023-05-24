bc5356d62611efc882b12bdf3172499d6abf2353
Ved Prakash <vedprakashsigh@gmail.com>
2023-05-24 13:54:18 +0530

    Site Completed, Working on Responsive Factors and Content for Site

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import { useEffect, useState } from "react";
+import projectData from "../../Data/project.json";
 
 const Project = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -12,18 +13,45 @@ const Project = () => {
     }, 3000);
   }, []);
 
+  const renderData = (projects) => {
+    return (
+      <div className="images-container">
+        {projects.map((project) => {
+          return (
+            <div className="image-box" key={project.idx}>
+              <img
+                className="project-image"
+                src={project.cover}
+                alt={project.title}
+              />
+              <div className="content">
+                <p className="title">{project.title}</p>
+                <h4 className="description"> {project.description} </h4>
+                <button
+                  className="btn"
+                  onClick={() => window.open(project.url)}
+                >
+                  VIEW
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <>
-      <div className="container project-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={["P", "r", "o", "j", "e", "c", "t", "s"]}
-              idx={15}
-            />
-          </h1>
-        </div>
+      <div className="container projects-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={["P", "r", "o", "j", "e", "c", "t", "s"]}
+            idx={15}
+          />
+        </h1>
+        <div>{renderData(projectData.projects)}</div>
       </div>
       <Loader type="ball-rotate" />
     </>
